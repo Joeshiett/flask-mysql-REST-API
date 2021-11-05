6e16924f85b8fb711f6b3529695f03f07d49e4da
joeshiett <josepheshiett@gmail.com>
2021-11-05 04:15:12 +0100

    New commit

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
-from flask import Flask, request, jsonify, make_response
+from flask import Flask, request, json, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
-from marshmallow import fields
-
+from marshmallow import fields, Schema
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://joseph:password@localhost/test'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-ma = Marshmallow(app)
 
 # Created a model titled "Author" which has the fields
 #ID, name and specialization. ID is self generated
@@ -16,31 +14,47 @@ class Author(db.Model):
     name = db.Column(db.String(20))
     specialization = db.Column(db.String(50))
 
+    def create(self):
+        db.session.add(self)
+        db.session.commit()
+        return self
+
     def __init__(self, name, specialization):
         self.name = name
         self.specialization = specialization
 
     def __repr__(self):
-        return '<Product %d>' % self.id
-db.create_all()
+        return '<Author %d>' % self.id
+
 
 #This class helps return json from SQLAlchemy
-class AuthorSchema(ma.SQLAlchemyAutoSchema):
+class AuthorSchema(Schema):
     class Meta:
         model = Author
         sqla_session = db.session
     
-    id = fields.Number(dump_only=True)
-    name = fields.String(required=True)
-    specialization = fields.String(required=True)
+    id = fields.Integer()
+    name = fields.String()
+    specialization = fields.String()
 
 # author route
 @app.route('/authors', methods=['GET'])
 def index():
     get_authors = Author.query.all()
     author_schema = AuthorSchema(many=True)
-    authors, error = author_schema.dump(get_authors)
+    authors = author_schema.dump(get_authors)
     return make_response(jsonify({"authors": authors}))
+
+@app.route('/authors', methods=['POST'])
+def create_author():
+    data = request.get_json(force=True)
+    new_author=Author(
+        name=data.get('name'),
+        specialization=data.get('specialization')
+    )
+    author_schema = AuthorSchema()
+    data = author_schema.dump(new_author)
+    return make_response(jsonify({'authors': data}),201)
 
 
 if __name__ == '__main__':
