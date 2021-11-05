1dfda562f3e331f2aaaf62ebf602a8f2ff6454cb
joeshiett <josepheshiett@gmail.com>
2021-11-05 13:15:56 +0100

    New commit

---
+++ src/__init__.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify
+import os
+from src.auth import auth
+from src.bookmarks import bookmarks
+from src.database import db
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+
+
+    if test_config is None:
+        app.config.from_mapping(
+            SECRET_KEY=os.environ.get("SECRET_KEY"),
+            SQLALCHEMY_DATABASE_URI=os.environ.get("SQLALCHEMY_DATABASE_URI"))
+    else:
+        app.config.from_mapping(test_config)
+
+    db.app=app
+    db.init_app(app)
+    app.register_blueprint(auth)
+    app.register_blueprint(bookmarks)
+
+    return app
