41be33538765b599d299c25cd9fb0ff4c188f989
joeshiett <josepheshiett@gmail.com>
2021-11-05 12:03:18 +0100

    New commit

--- app.py
+++ app.py
@@ -1,60 +1,14 @@
-from flask import Flask, request, json, jsonify, make_response
-from flask_sqlalchemy import SQLAlchemy
-from marshmallow import fields, Schema
+from flask import Flask, jsonify
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://joseph:password@localhost/test'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
 
-# Created a model titled "Author" which has the fields
-#ID, name and specialization. ID is self generated
-class Author(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20))
-    specialization = db.Column(db.String(50))
-
-    def create(self):
-        db.session.add(self)
-        db.session.commit()
-        return self
-
-    def __init__(self, name, specialization):
-        self.name = name
-        self.specialization = specialization
-
-    def __repr__(self):
-        return '<Author %d>' % self.id
-
-
-#This class helps return json from SQLAlchemy
-class AuthorSchema(Schema):
-    class Meta:
-        model = Author
-        sqla_session = db.session
-    
-    id = fields.Integer()
-    name = fields.String()
-    specialization = fields.String()
-
-# author route
-@app.route('/authors', methods=['GET'])
+@app.get('/')
 def index():
-    get_authors = Author.query.all()
-    author_schema = AuthorSchema(many=True)
-    authors = author_schema.dump(get_authors)
-    return make_response(jsonify({"authors": authors}))
+    return jsonify({'message': 'Welcome to Flask!'})
 
-@app.route('/authors', methods=['POST'])
-def create_author():
-    data = request.get_json()
-    new_author=Author(
-        name=data.get('name'),
-        specialization=data.get('specialization')
-    )
-    author_schema = AuthorSchema()
-    data = author_schema.dump(new_author)
-    return make_response(jsonify({'authors': data}),201)
+@app.get('/hello')
+def hello():
+    return jsonify({'message': 'Hello World'})
 
 
 if __name__ == '__main__':
